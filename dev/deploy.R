9fb77746b2acf7f1357ef821ef05a171eab95b41
Jimmy Briggs <jimmy.briggs@tychobra.com>
2021-03-12 18:49:17 -0500

    add github and dockerhub registries to deployment

--- dev/deploy.R
+++ dev/deploy.R
@@ -16,6 +16,17 @@ system("docker build -t powwater_vendorsdashboard .")
 system("docker run --env SHINY_LOG_STDERR=1 --rm -p 8080:8080 powwater_vendorsdashboard")
 browseURL("localhost:8080")
 
+# tag and push to github dockerhub
+system("cat ~/.docker/github_PAT.txt | docker login https://docker.pkg.github.com -u USERNAME --password-stdin")
+system("docker tag powwater_vendorsdashboard docker.pkg.github.com/powwater/vendor_dashboard/powwater_vendorsdashboard:alpha")
+system("docker build -t docker.pkg.github.com/powwater/vendor_dashboard/powwater_vendorsdashboard:alpha .")
+system("docker push docker.pkg.github.com/powwater/vendor_dashboard/powwater_vendorsdashboard:alpha")
+
+# tag and push to dockerhub
+system("docker tag powwater_vendorsdashboard jimbrig2011/powwater_vendorsdashboard:alpha")
+system("docker build -t jimbrig2011/powwater_vendor_dashboard:alpha .")
+system("docker push")
+
 # build production, tag, and push to GCR
 system("gcloud auth configure-docker")
 system("docker build --build-arg R_CONFIG_ACTIVE=production -t powwater_vendorsdashboard .")
