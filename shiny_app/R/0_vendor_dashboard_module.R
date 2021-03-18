aade98e59a6a2b72bae975eed7c5d13b7070d5cc
Jimmy Briggs <jimmy.briggs@tychobra.com>
2021-03-18 19:19:02 -0400

    #31 - added vendors region UI to dashboard module

--- shiny_app/R/0_vendor_dashboard_module.R
+++ shiny_app/R/0_vendor_dashboard_module.R
@@ -3,13 +3,34 @@ vendor_dashboard_ui <- function(id) {
   ns <- NS(id)
 
   tagList(
-
+    fluidRow(
+      column(
+        6,
+        h5("Region:"),
+        uiOutput(ns("vendor_region"))
+      ),
+      column(
+        6,
+        h5("Vendor Details:") #,
+        # googleway::google_mapOutput(ns("customer_locations"))
+      )
+    )
   )
-
 }
 
 vendor_dashboard <- function(input, output, session, vendor_info) {
 
+  output$vendor_region <- renderUI({
+    HTML(
+      paste0(
+        '<iframe width="100%" height="450" style="border:0" loading="lazy" allowfullscreen
+      src="https://www.google.com/maps/embed/v1/place?q=place_id:',
+      vendor_info()$region_id, '&key=', key, '"></iframe>'
+      )
+    )
+  })
+
+  # top customers
 
 
 }
