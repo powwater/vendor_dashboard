a91735dffbeb7d941ff6f79f079fc7e264534f51
Jimmy Briggs <jimmy.briggs@tychobra.com>
2021-04-01 11:16:02 -0400

    #64 - change to shinySetInputValue in JS

--- shiny_app/www/js/is_mobile.js
+++ shiny_app/www/js/is_mobile.js
@@ -1,4 +1,4 @@
 $(document).on('shiny:sessioninitialized', function (e) {
   var mobile = window.matchMedia("only screen and (max-width: 768px)").matches;
-  Shiny.onInputChange('is_mobile_device', mobile);
+  Shiny.setInputValue('is_mobile_device', mobile);
 });
