b3f3d8cad228cc18985390b2928f5f12d22db274
Jimmy Briggs <jimmy.briggs@tychobra.com>
2021-04-08 18:06:46 -0400

    add vendor specific riders table

--- shiny_app/R/5_riders_module.R
+++ shiny_app/R/5_riders_module.R
@@ -10,11 +10,22 @@ riders_module_ui <- function(id){
         solidHeader = TRUE,
         br(),
         br(),
-        column(
-          width = 12,
-          DT::DTOutput(ns('riders_table')) %>%
-            shinycustomloader::withLoader(),
-          hr()
+        fluidRow(
+          column(
+            width = 12,
+            h3("Powwater Riders:"),
+            DT::DTOutput(ns('pow_riders_table')) %>%
+              shinycustomloader::withLoader()
+          )
+        ),
+        hr(),
+        fluidRow(
+          column(
+            12,
+            h3("Vendor Specific Riders:"),
+            DT::DTOutput(ns('vendor_riders_table')) %>%
+              shinycustomloader::withLoader()
+          )
         )
       )
     )
@@ -24,9 +35,6 @@ riders_module_ui <- function(id){
 riders_module <- function(input, output, session, vendor_info, is_mobile) {
 
   riders <- reactive({
-
-    # id <- notify("Loading Riders from Database...")
-    # on.exit(shinyFeedback::hideToast(), add = TRUE)
 
     vend <- vendor_info()$vendor_uid
 
@@ -43,22 +51,41 @@ riders_module <- function(input, output, session, vendor_info, is_mobile) {
     out
   })
 
-  riders_prep <- reactiveVal(NULL)
+  pow_riders <- reactive({
+    riders()$pow_riders
+  })
 
-  observeEvent(riders(), {
+  pow_riders_prep <- reactiveVal(NULL)
 
-    hold <- riders() %>%
+  observeEvent(pow_riders(), {
+
+    hold <- pow_riders() %>%
       select(-rider_uid, -vendor_uid, -c(created_at:modified_by))
 
-    riders_prep(hold)
+    pow_riders_prep(hold)
 
   })
 
-  output$riders_table <- DT::renderDT({
+  vendor_riders_prep <- reactiveVal(NULL)
 
-    req(riders_prep())
+  vendor_riders <- reactive({
+    riders()$vendor_riders
+  })
 
-    out <- riders_prep()
+  observeEvent(vendor_riders(), {
+
+    hold <- vendor_riders() %>%
+      select(-rider_uid, -vendor_uid, -c(created_at:modified_by))
+
+    vendor_riders_prep(hold)
+
+  })
+
+  output$pow_riders_table <- DT::renderDT({
+
+    req(pow_riders_prep())
+
+    out <- pow_riders_prep()
 
     cols <- snakecase::to_title_case(colnames(out))
 
@@ -92,7 +119,56 @@ riders_module <- function(input, output, session, vendor_info, is_mobile) {
         columnDefs = list(
           list(className = "dt-center dt-col", targets = "_all")
         ),
-        buttons = dt_bttns(out, "riders-table")
+        buttons = dt_bttns(out, "riders-table"),
+        language = list(
+          emptyTable = "Selected Vendor does not have any Riders assigned from Powwater."
+        )
+      )
+    )
+  })
+
+  output$vendor_riders_table <- DT::renderDT({
+
+    req(vendor_riders_prep())
+
+    out <- vendor_riders_prep()
+
+    cols <- snakecase::to_title_case(colnames(out))
+
+    if (isTRUE(is_mobile())) {
+      tbl_class <- "table table-striped table-bordered dt-center dt-responsive dt-compact dt-hover nowrap table"
+      tbl_exts <- c("Buttons", "Responsive")
+      tbl_filt <- "none"
+      tbl_scroll <- FALSE
+    } else {
+      tbl_class <- "table table-striped table-bordered dt-center dt-compact dt-hover nowrap table"
+      tbl_exts <- c("Buttons")
+      tbl_filt <- "top"
+      tbl_scroll <- TRUE
+    }
+
+    DT::datatable(
+      out,
+      style = "bootstrap",
+      rownames = FALSE,
+      colnames = cols,
+      selection = "none",
+      class = tbl_class,
+      extensions = tbl_exts,
+      filter = tbl_filt,
+      width = "100%",
+      options = list(
+        scrollX = tbl_scroll,
+        dom = "<'row'<'col-sm-3'l><'col-sm-6 text-center'B><'col-sm-3'f>>
+               <'row'<'col-sm-12'tr>>
+               <'row'<'col-sm-5'i><'col-sm-7'p>>",
+        columnDefs = list(
+          list(className = "dt-center dt-col", targets = "_all")
+        ),
+        buttons = dt_bttns(out, "riders-table"),
+        language = list(
+          emptyTable = "Selected Vendor does not have any Vendor specific Riders."
+        )
       )
     )
   })
