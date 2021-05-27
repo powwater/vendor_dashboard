816c6b6d982c39766a335d16e16476f4403e48a8
merlinoa <andy.merlino@tychobra.com>
2021-05-27 16:00:20 -0400

    removed old "dev/" files

--- dev/run_background_app.R
+++
@@ -1,7 +0,0 @@
-run_app <- function() {
-  wd <- rprojroot::find_root(rprojroot::has_file_pattern(".Rproj"))
-  rstudioapi::jobRunScript("dev/shiny_run.R", workingDir = wd)
-  shiny::onSessionEnded(function() rstudioapi::jobRemove("shiny_run.R"))
-}
-
-run_app()
--- dev/use_template.R
+++
@@ -1,6 +0,0 @@
-#' @importFrom whisker whisker.render
-use_template <- function(template_path, out_path, data = list()) {
-  contents <- strsplit(whisker::whisker.render(usethis:::read_utf8(template_path), data), "\n")[[1]]
-  usethis:::write_over(out_path, contents)
-  file.edit(out_path)
-}
